e4b4e092655a3660dca647dd60960b0a06356adc
DroidIvan <ivanthenintendogamer@gmail.com>
2020-02-27 01:55:14 +0100

    Add files via upload

---
+++ menu.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { ObservableMedia, MediaChange } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent implements OnInit {
+  opened = true;
+  over = 'side';
+  expandHeight = '42px';
+  collapseHeight = '42px';
+  displayMode = 'flat';
+  // overlap = false;
+
+  watcher: Subscription;
+
+  constructor(media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      if (change.mqAlias === 'sm' || change.mqAlias === 'xs') {
+        this.opened = false;
+        this.over = 'over';
+      } else {
+        this.opened = true;
+        this.over = 'side';
+      }
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+}
