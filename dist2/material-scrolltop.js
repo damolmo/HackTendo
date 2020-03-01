3295d24748f59385380e29ab3303d0a6fb125e3b
DroidIvan <ivanthenintendogamer@gmail.com>
2020-03-01 21:58:25 +0100

    Update material-scrolltop.js

--- dist2/material-scrolltop.js
+++ dist2/material-scrolltop.js
@@ -7,62 +7,109 @@
  * Repo: https://github.com/bartholomej/material-scrollTop
  * Issues: https://github.com/bartholomej/material-scrollTop/issues
  */
-(function ($) {
-    function mScrollTop(element, settings) {
-        var _ = this;
-        var breakpoint;
-        var scrollTo = 0;
-        _.btnClass = '.material-scrolltop';
-        _.revealClass = 'reveal';
-        _.btnElement = $(_.btnClass);
-        _.initial = {
-            revealElement: 'body',
-            revealPosition: 'top',
-            padding: 0,
-            duration: 600,
-            easing: 'swing',
-            onScrollEnd: null
-        };
-        _.options = $.extend({}, _.initial, settings);
-        _.revealElement = $(_.options.revealElement);
-        breakpoint =
-            _.options.revealPosition !== 'bottom'
-                ? _.revealElement.offset().top
-                : _.revealElement.offset().top + _.revealElement.height();
-        scrollTo = element.offsetTop + _.options.padding;
-        $(document).scroll(function () {
-            if (breakpoint < $(document).scrollTop()) {
-                _.btnElement.addClass(_.revealClass);
-            }
-            else {
-                _.btnElement.removeClass(_.revealClass);
-            }
-        });
-        _.btnElement.click(function () {
-            var trigger = true;
-            $('html, body').animate({
-                scrollTop: scrollTo
-            }, _.options.duration, _.options.easing, function () {
-                if (trigger) {
-                    // Fix callback triggering twice on chromium
-                    trigger = false;
-                    var callback = _.options.onScrollEnd;
-                    if (typeof callback === 'function') {
-                        callback();
-                    }
-                }
-            });
-            return false;
-        });
-    }
-    $.fn.materialScrollTop = function () {
-        var _ = this;
-        var opt = arguments[0];
-        var l = _.length;
-        var i = 0;
-        if (typeof opt == 'object' || typeof opt == 'undefined') {
-            _[i].materialScrollTop = new mScrollTop(_[i], opt);
-        }
-        return _;
-    };
-})(jQuery);
+function darkbutton() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+	  var element = document.getElementById("dark-waves");
+  element.classList.toggle("dark-waves");
+		  var element = document.getElementById("menu-side");
+  element.classList.toggle("menu-side");
+	
+	
+	  var element = document.getElementById("home");
+  element.classList.toggle("home");
+	
+	  var element = document.getElementById("google");
+  element.classList.toggle("google");
+	
+	  var element = document.getElementById("android");
+  element.classList.toggle("android");
+	
+	  var element = document.getElementById("n3ds");
+  element.classList.toggle("n3ds");
+	  var element = document.getElementById("nswitch");
+  element.classList.toggle("nswitch");
+	
+	  var element = document.getElementById("nwiiu");
+  element.classList.toggle("nwiiu");
+	  var element = document.getElementById("ndsi");
+  element.classList.toggle("ndsi");
+	  var element = document.getElementById("nwiimini");
+  element.classList.toggle("nwiimini");
+	
+	
+	 var element = document.getElementById("hhome");
+  element.classList.toggle("hhome");
+	 var element = document.getElementById("aandroid");
+  element.classList.toggle("aandroid");
+	 var element = document.getElementById("nn3ds");
+  element.classList.toggle("nn3ds");
+	 var element = document.getElementById("nuno");
+  element.classList.toggle("nuno");
+	 var element = document.getElementById("ndos");
+  element.classList.toggle("ndos");
+	 var element = document.getElementById("ntres");
+  element.classList.toggle("ntres");
+	 var element = document.getElementById("ncuatro");
+  element.classList.toggle("ncuatro");
+	 var element = document.getElementById("ncinco");
+  element.classList.toggle("ncinco");
+	 var element = document.getElementById("nseis");
+  element.classList.toggle("nseis");
+	 var element = document.getElementById("nsiete");
+  element.classList.toggle("nsiete");
+var element = document.getElementById("ncero");
+  element.classList.toggle("ncero");
+
+	 var element = document.getElementById("nocho");
+  element.classList.toggle("nocho");
+	 var element = document.getElementById("nnueve");
+  element.classList.toggle("nnueve");
+	 var element = document.getElementById("ndiez");
+  element.classList.toggle("ndiez");
+	 var element = document.getElementById("nveinte");
+  element.classList.toggle("nveinte");
+	 var element = document.getElementById("ntreinta");
+  element.classList.toggle("ntreinta");
+	 var element = document.getElementById("ncuarenta");
+  element.classList.toggle("ncuarenta");
+	 var element = document.getElementById("ncincuenta");
+  element.classList.toggle("ncincuenta");
+	 var element = document.getElementById("nsesenta");
+  element.classList.toggle("nsesenta");
+
+
+	 var element = document.getElementById("nsetenta");
+  element.classList.toggle("nsetenta");
+	 var element = document.getElementById("nochenta");
+  element.classList.toggle("nochenta");
+	 var element = document.getElementById("nnoventa");
+  element.classList.toggle("nnoventa");
+	 var element = document.getElementById("ncien");
+  element.classList.toggle("ncien");
+	 var element = document.getElementById("ndoscientos");
+  element.classList.toggle("ndoscientos");
+	 var element = document.getElementById("ntrescientos");
+  element.classList.toggle("ntrescientos");
+	 var element = document.getElementById("ncuatrocientos");
+  element.classList.toggle("ncuatrocientos");
+	 var element = document.getElementById("nquinientos");
+  element.classList.toggle("nquinientos");
+
+	var element = document.getElementById("nn");
+  element.classList.toggle("nn");
+	 var element = document.getElementById("nnn");
+  element.classList.toggle("nnn");
+	 var element = document.getElementById("nnnn");
+  element.classList.toggle("nnnn");
+	 var element = document.getElementById("nnnnn");
+  element.classList.toggle("nnnnn");
+
+var element = document.getElementsByClassName("divider");
+  element.classList.toggle("divider")
+
+var element = document.getElementsByClassName("w3-white");
+  element.classList.toggle("w3-white")
+
+}
+    
