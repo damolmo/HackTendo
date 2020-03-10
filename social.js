cd471fec80bb799f1db00fd311e2f4dedba6c889
DroidIvan <ivanthenintendogamer@gmail.com>
2020-03-10 02:39:57 +0100

    Delete social.js

--- social.js
+++
@@ -1,9 +0,0 @@
-function share() {
-    var hidden = $("#social_btn").css("display") == "none";
-    if (hidden) {
-        $("#social_btn").slideDown(500).css("display", "inline-block");
-        console.log("sucess");
-    } else {
-        $("#social_btn").slideUp(500);
-    }
-}
