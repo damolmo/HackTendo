2be067f8116ba9f709ace6e7f77087a00addcf72
DroidIvan <ivanthenintendogamer@gmail.com>
2020-05-23 11:29:20 +0200

    Update main.js

--- player/js/main.js
+++ player/js/main.js
@@ -48,6 +48,52 @@ $(document).ready(function () {
             tl.resume();
 
         }
+        
+        }else {
+                             
+                            if ($('.player .player__albumImg.active-song').is(':last-child')) {
+
+            $('.player__albumImg.active-song').removeClass('active-song');
+
+            $('.player .player__albumImg:first-child').addClass('active-song');
+
+            audioElement.addEventListener("timeupdate", function () {
+
+                var duration = this.duration;
+
+                var currentTime = this.currentTime;
+
+                var percentage = (currentTime / duration) * 100;
+
+                playhead.style.width = percentage + '%';
+
+            });
+
+        } else {
+
+            $('.player__albumImg.active-song').removeClass('active-song').next().addClass('active-song');
+
+            audioElement.addEventListener("timeupdate", function () {
+
+                var duration = this.duration;
+
+                var currentTime = this.currentTime;
+
+                var percentage = (currentTime / duration) * 100;
+
+                playhead.style.width = percentage + '%';
+
+            });
+
+        }
+
+        updateInfo();
+
+        audioElement.setAttribute('src', $('.active-song').attr('data-src'));
+
+        audioElement.play(); 
+                             
+                             })
 
     });
     
