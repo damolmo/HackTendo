4f48daaec8aa17c61793f9f41f4bfef9fdd5b0c6
David <davidmolinamorales1999@gmail.com>
2021-03-25 23:43:00 +0100

    Update ui.js

--- fastboot/demo/ui.js
+++ fastboot/demo/ui.js
@@ -1,7 +1,7 @@
 // @license magnet:?xt=urn:btih:d3d9a9a6595521f9666a5e94cc830dab83b65699&dn=expat.txt MIT
 
 import * as fastboot from "../dist/fastboot.mjs";
-import { BlobStore } from "./download.js";
+import { BlobStore } from "download.js";
 
 let device = new fastboot.FastbootDevice();
 let blobStore = new BlobStore();
